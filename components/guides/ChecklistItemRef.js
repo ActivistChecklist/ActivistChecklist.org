683e7c51a27109a81c8262c038460998d87c681e
ActivistChecklist <contact@activistchecklist.org>
2025-06-12 21:27:29 -0700

    Add checklist reference items to allow reuse (#18)
    * WIP reference items
    
    * Update caniuse

---
+++ components/guides/ChecklistItemRef.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storyblokEditable } from '@storyblok/react';
+import ChecklistItem from './ChecklistItem';
+
+const ChecklistItemRef = ({ blok, expandTrigger, index }) => {
+
+  // The reference_item is already resolved by Storyblok's resolve_relations
+  const referencedItem = blok.reference_item;
+
+  
+  if (!referencedItem) {
+    return (
+      <div className="mb-4 p-4 text-muted-foreground" {...storyblokEditable(blok)}>
+        No reference item found
+      </div>
+    );
+  }
+
+  // Render the referenced item using the ChecklistItem component
+  return (
+    <ChecklistItem 
+      blok={referencedItem.content}
+      expandTrigger={expandTrigger}
+      index={index}
+      editable={true}
+    />
+  );
+};
+
+export default ChecklistItemRef; 
